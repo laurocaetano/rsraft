e9be8e81465e47c473771dcca621a60c7251a548
Lauro Caetano <laurocaetano1@gmail.com>
2021-01-09 02:30:00 +0100

    Initial basic structure for implementing the leader election
    A server always starts as a follower, and with the term set to 1.

---
+++ src/raft/core.rs
@@ -0,0 +1,37 @@
+use uuid::Uuid;
+
+#[derive(Debug, PartialEq)]
+enum State {
+    FOLLOWER,
+    LEADER,
+    CANDIDATE,
+}
+
+pub struct Server {
+    id: Uuid,
+    state: State,
+    term: u64,
+}
+
+impl Server {
+    fn new() -> Self {
+        Server {
+            id: Uuid::new_v4(),
+            state: State::FOLLOWER,
+            term: 1,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_server() {
+        let server = Server::new();
+
+        assert_eq!(server.term, 1);
+        assert_eq!(server.state, State::FOLLOWER);
+    }
+}

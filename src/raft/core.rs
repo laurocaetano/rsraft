6b4f0775a71a5ea73d1a3b30b3796777d88560ac
Lauro Caetano <laurocaetano1@gmail.com>
2021-01-10 02:16:11 +0100

    Add more struct definitions for the basic Raft algorithms

--- src/raft/core.rs
+++ src/raft/core.rs
@@ -1,3 +1,4 @@
+use std::net::Ipv4Addr;
 use uuid::Uuid;
 
 #[derive(Debug, PartialEq)]
@@ -7,10 +8,33 @@ enum State {
     CANDIDATE,
 }
 
-pub struct Server {
+enum LogEntry {
+    HEARTBEAT,
+}
+
+struct Peer {
+    address: Ipv4Addr,
+    term: u64,
+    state: State,
+}
+
+struct Server {
     id: Uuid,
     state: State,
     term: u64,
+    peers: Vec<Peer>,
+    log_entries: Vec<LogEntry>,
+    voted_for: Option<Peer>,
+}
+
+struct RequestVoteRequestRpc {
+    term: u64,
+    candidate_id: Uuid,
+}
+
+struct RequestVoteResponseRpc {
+    term: u64,
+    vote_granted: bool,
 }
 
 impl Server {
@@ -19,6 +43,9 @@ impl Server {
             id: Uuid::new_v4(),
             state: State::FOLLOWER,
             term: 0,
+            peers: Vec::new(),
+            log_entries: Vec::new(),
+            voted_for: None,
         }
     }
 }
@@ -33,5 +60,8 @@ mod tests {
 
         assert_eq!(server.term, 0);
         assert_eq!(server.state, State::FOLLOWER);
+        assert_eq!(server.peers.len(), 0);
+        assert_eq!(server.log_entries.len(), 0);
+        assert!(server.voted_for.is_none());
     }
 }
